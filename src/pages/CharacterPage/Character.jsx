f525b48df99504c0aacf2edd09aab1e449ec4e5e
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2022-06-08 21:06:16 +0300

    Corrected name of personal information.

--- src/pages/CharacterPage/Character.jsx
+++ src/pages/CharacterPage/Character.jsx
@@ -38,7 +38,7 @@ const Character = () => {
                         Location: {person.location.name}
                     </div>
                     <div>
-                        First episode: {person.episode[0].slice(-1)} episode
+                        First appearance: {person.episode[0].slice(-1)} episode
                     </div>
                     <div>
                         Created: {new Date(person.created).toLocaleDateString()}
